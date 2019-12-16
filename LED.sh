ff5b02ae544e69fdaff8c52510eec14ea2464af7
Vincent <imshydow@gmail.com>
2019-12-16 15:38:18 -0500

    Update LED.sh

--- LED.sh
+++ LED.sh
@@ -2,6 +2,7 @@
 # Initialisation des GPIOs. Les GPIOs nécessaires sont activés avec la commande export, puis la direction est choisie.
 if [ $(cat /sys/class/gpio/gpio71/direction) -eq out && $(class/gpio/gpio233/direction) -eq out ]
 	then
+	echo pas besoin d initier 
 	else
 		echo "71" > /sys/class/gpio/export
 		echo "out" > /sys/class/gpio/gpio71/direction
